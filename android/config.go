613509439b1d04c9a21eaaf03d640c561e2d833b
Simon Shields <simon@lineageos.org>
2017-09-04 21:09:44 +1000

    soong: explicitly specify name of Aospa variables struct
    Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- android/config.go
+++ android/config.go
@@ -847,13 +847,13 @@ func stringSlice(s *[]string) []string {
 }
 
 func (c *deviceConfig) QTIAudioPath() string {
-	return String(c.config.productVariables.QTIAudioPath)
+	return String(c.config.productVariables.Aospa.QTIAudioPath)
 }
 
 func (c *deviceConfig) QTIDisplayPath() string {
-	return String(c.config.productVariables.QTIDisplayPath)
+	return String(c.config.productVariables.Aospa.QTIDisplayPath)
 }
 
 func (c *deviceConfig) QTIMediaPath() string {
-	return String(c.config.productVariables.QTIMediaPath)
+	return String(c.config.productVariables.Aospa.QTIMediaPath)
 }
